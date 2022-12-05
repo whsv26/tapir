78af72bb01c9c5d19525646d978a1d84c95bb276
whsv26 <whsv26@gmail.com>
2022-12-05 04:39:26 +0300

    Mediator test app.

--- src/main/scala/util/bus/package.scala
+++ src/main/scala/util/bus/package.scala
@@ -1,51 +1,15 @@
 package org.whsv26.tapir
 package util
 
-import cats.Id
-import cats.effect.kernel.{Async, Sync}
-import fs2.{Pipe, Stream}
-import fs2.concurrent.Topic
-import org.whsv26.tapir.util.bus.Request.Aux
+import cats.effect.implicits.genSpawnOps
+import cats.effect.kernel.Async
 import cats.syntax.functor._
-import cats.syntax.traverse._
+import fs2.concurrent.Topic
+import fs2.{Pipe, Stream}
 
 import scala.reflect.ClassTag
 
 package object bus {
-
-  trait Mediator[F[_]] {
-    def send[Out, Req <: Request.Aux[Out]](request: Req): F[Out]
-    def publish(notification: Notification): F[Unit]
-  }
-
-  object Mediator {
-    class Impl[F[_]: Async](
-      requestHandlers: List[RequestHandlerBase[F]],
-      notificationHandlers: List[NotificationHandlerBase[F]],
-      notificationTopic: Topic[F, Notification]
-    ) extends Mediator[F] {
-
-      override def send[Out, Req <: Request.Aux[Out]](request: Req): F[Out] =
-        requestHandlers.collectFirst {
-          // TODO: Is it working? Why no type tag needed?
-          // TODO: Optimize O(n) search
-          case handler: RequestHandler[F, Req] => handler.handle(request)
-        }.head
-
-      override def publish(notification: Notification): F[Unit] =
-        notificationTopic.publish1(notification).void
-
-      def start: F[Unit] =
-        Stream
-          .emits(notificationHandlers)
-          .map(_.handler)
-          .map(handler => notificationTopic.subscribe(20).through(handler))
-          .parJoinUnbounded
-          .compile
-          .drain
-    }
-  }
-
   trait Notification
 
   trait Request {
@@ -58,18 +22,20 @@ package object bus {
 
   trait QueryRequest extends Request
 
-  trait CommandRequest extends Request
-
   trait Query[T] extends QueryRequest {
     override type Out = T
   }
+
+  trait CommandRequest extends Request
 
   trait Command[T] extends CommandRequest {
     override type Out = T
   }
 
   trait NotificationHandlerBase[F[_]] {
-    type In <: Request
+    type In <: Notification
+
+    implicit def tag: ClassTag[In]
 
     def handle: In => F[Unit]
 
@@ -77,24 +43,74 @@ package object bus {
       _.collect { case n: In => n }.evalMap(handle)
   }
 
+  abstract class NotificationHandler[F[_], N <: Notification](implicit
+    notificationTag: ClassTag[N]
+  ) extends NotificationHandlerBase[F] {
+    override type In = N
+    override implicit def tag: ClassTag[In] = notificationTag
+  }
+
   trait RequestHandlerBase[F[_]] {
     type In <: Request
     type Out = In#Out
+    def tag: ClassTag[In]
     def handle: In => F[Out]
   }
 
-  trait RequestHandler[F[_], Req <: Request] extends RequestHandlerBase[F] {
-    override type In = Req
+  trait RequestHandler[F[_], R <: Request] extends RequestHandlerBase[F] {
+    override type In = R
   }
 
-  trait QueryHandler[F[_], In <: QueryRequest] extends RequestHandler[F, In]
+  abstract class QueryHandler[F[_], Q <: QueryRequest](implicit
+    queryTag: ClassTag[Q]
+  ) extends RequestHandler[F, Q] {
+    override def tag: ClassTag[Q] = queryTag
+  }
 
-  trait CommandHandler[F[_], In <: CommandRequest] extends RequestHandler[F, In]
+  abstract class CommandHandler[F[_], C <: CommandRequest](implicit
+    commandTag: ClassTag[C]
+  ) extends RequestHandler[F, C] {
+    override def tag: ClassTag[C] = commandTag
+  }
 
-  case class MyQuery(i: Int) extends Query[Boolean]
+  trait Mediator[F[_]] {
+    def send[Out](request: Request.Aux[Out]): F[Out]
+    def publish(notification: Notification): F[Unit]
+  }
 
-  class MyHandler extends QueryHandler[Id, MyQuery] {
-    override def handle =
-      query => query.i > 0
+  object Mediator {
+    class Impl[F[_]: Async](
+      notificationTopic: Topic[F, Notification],
+      notificationHandlers: List[NotificationHandlerBase[F]],
+      requestHandlers: List[RequestHandlerBase[F]],
+    ) extends Mediator[F] {
+
+      private val requestHandlersMap = requestHandlers.map {
+        handler => handler.tag.runtimeClass -> handler
+      }.toMap
+
+      override def send[Out](request: Request.Aux[Out]): F[Out] =
+        requestHandlersMap(request.getClass)
+          .asInstanceOf[RequestHandler[F, Request.Aux[Out]]]
+          .handle(request)
+
+      override def publish(notification: Notification): F[Unit] =
+        notificationTopic.publish1(notification).void
+
+      def start: F[Unit] =
+        Stream
+          .emits(notificationHandlers)
+          .map(_.handler)
+          .map { handler =>
+            notificationTopic
+              .subscribe(20)
+              .through(handler)
+          }
+          .parJoinUnbounded
+          .compile
+          .drain
+          .start
+          .void
+    }
   }
 }
